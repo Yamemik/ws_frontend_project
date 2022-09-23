3018aa270413376c17b69f820adb5454e6cda145
Yamemik <kuancarlos@mail.ru>
2022-09-23 17:24:26 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('.slider_inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
+    });
+
+
+});
